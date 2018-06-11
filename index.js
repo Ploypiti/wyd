c748f84290758a55a03b6e8924468eea404facd8
Ploypiti Piyaprapan <ploypitisweet@gmail.com>
2018-06-11 10:50:27 +0700

    debug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 var path = require('path');
+var port = process.env.PORT || 8000
 
 app.use(express.static('public'))
 
@@ -8,4 +9,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(8080);
+app.listen(port, function() {
+    console.log("WYD is running on port " + port + "!!!");
+});
